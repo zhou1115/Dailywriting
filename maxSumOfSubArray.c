a3a83f7ba193850159f238ca876ddae748eb0b09
zhouk1115 <kangzhoutai@gmail.com>
2020-12-11 00:36:06 +0800

    first commit

---
+++ maxSumOfSubArray.c
@@ -0,0 +1,30 @@
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+const int maxn=1010;
+int A[maxn],dp[maxn];
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&A[i]);
+	}
+	//bianjie
+	dp[0]=A[0];
+	for(int i=1;i<n;i++)
+	{
+		dp[i]=max(A[i],dp[i-1]+A[i]);
+	}
+	int k=0;
+	for(int i=1;i<n;i++)
+	{
+		if(dp[i]>dp[k])
+		{
+			k=i;
+		}
+	}
+	printf("%d\n",dp[k]);
+	return 0;
+}
